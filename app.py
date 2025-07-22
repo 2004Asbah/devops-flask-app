5e263696eb61f69d6ee778e2fbd9ef12d9bbb2c7
Asbah Rehman <asbahrehman123@gmail.com>
2025-07-23 00:16:44 +0530

    Fix: formatted app.py and excluded myenv from linter

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from Asbah's Flask DevOps App!"
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About Page'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(debug=True)
